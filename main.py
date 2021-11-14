bacff532ef0460a253f4680f554e483089020243
Breketov <57558717+Breketov@users.noreply.github.com>
2021-11-14 17:50:23 +0300

    Пока сейв

--- main.py
+++ main.py
@@ -75,10 +75,49 @@ AB = rand_AB(Nmax)
 t = np.linspace(0, 1, Nmax)
 x = strategy(t, AB[0], AB[1]) """
 
-
+""" 
 data_AB = pd.read_csv('data_AB.csv')
 
 Ay = data_AB["A_yng"].tolist()
 By = data_AB["B_yng"].tolist()
 print(Ay)
-print(By)
+print(By) """
+
+alpha_y = 0.0016
+alpha_o = 0.006
+betta_y = 0.0000007
+betta_o = 0.000000075
+gamma_y = 0.00008
+gamma_o = 0.004
+delta_y = 0.000016
+delta_o = 0.00006
+
+D = 120
+D0 = 70  
+sigma1 = 1
+sigma2 = 1
+Nmax = 100
+
+data_M = pd.read_csv('data_M.csv')
+M1 = data_M['M1'].tolist()
+M2 = data_M['M2'].tolist()
+M3 = data_M['M3'].tolist()
+M4 = data_M['M4'].tolist()
+M5 = data_M['M5'].tolist()
+M6 = data_M['M6'].tolist()
+M7 = data_M['M7'].tolist()
+M8 = data_M['M8'].tolist()
+J = []
+for i in range(0, 10):
+   r = 0
+   s = 0
+   p = 0
+   q = 0
+   r = alpha_o*M5[i] + betta_o*M7[i] + delta_o*M8[i]
+   s = gamma_o*M6[i]
+   p = alpha_y*M1[i] + betta_y*M3[i] + delta_y*M4[i]
+   q = gamma_y*M2[i]
+   j_ =  -s - p - q + np.sqrt(4*r*p + (p + q - s)**2)
+   J.append(j_)
+
+print(J.sort())
