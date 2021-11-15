7371efbd7204df51933d3bd427fad0b24cf44ad7
Breketov <57558717+Breketov@users.noreply.github.com>
2021-11-15 21:55:51 +0300

    Фикс

--- main.py
+++ main.py
@@ -96,8 +96,10 @@ D = 120
 D0 = 70  
 sigma1 = 1
 sigma2 = 1
-Nmax = 100
+Nmax = 1000
 
+
+""" 
 data_M = pd.read_csv('data_M.csv')
 M1 = data_M['M1'].tolist()
 M2 = data_M['M2'].tolist()
@@ -121,3 +123,120 @@ for i in range(0, 10):
    J.append(j_)
 
 print(J.sort())
+ """
+
+""" 
+sigm1 = 0.25
+sigm2 = 0.003
+C0 = 60
+C = 140
+#lam = [0.1 / sigm1, 1 / sigm2, 0.000025, 0.1]
+#lam = [0.5 / sigm1, 1 / sigm2, 0.000025, 0.01]
+lam = [0.5, 1, 0.000025, 0.01]
+
+from random import randint
+import pandas as pd
+
+def gen_AB():
+    A = randint(-C * 100, 0)
+    B1 = randint(0, min(-A, A + C * 100))
+    B2 = -B1
+    return A / 100, B1 / 100, B2 / 100 
+
+def get_strategy_of_behavior(N):
+    arr = []
+    for i in range(N // 2):
+        A, B1, B2 = gen_AB()
+        while (A, B1) in arr:
+            A, B1, B2 = gen_AB()
+        arr.append((A, B1))
+        arr.append((A, B2))
+    data = pd.DataFrame(columns=['A', 'B'], data = arr)
+    data['C'] = C
+    data['C0'] = C0
+    return data
+
+def get_strategy_of_behavior_1(N):
+    n = int(N ** 0.5)
+    arr = []
+    for i in range(n):
+        for j in range(n):
+            A = -C + C * i / n
+            B1 =  min(A + C, -A) * j / n
+            B2 = -B1
+            if (A, B1) not in arr:
+                arr.append((A, B1))
+            if (A, B2) not in arr:
+                arr.append((A, B2))
+    data = pd.DataFrame(columns=['A', 'B'], data = arr)
+    data['C'] = C
+    data['C0'] = C0
+    return data
+        
+strat_b = get_strategy_of_behavior_1(30)
+print(strat_b)  
+
+ """
+
+
+
+
+""" 
+
+#_______________________________________________________________________________________________________________________-
+# Тут типо дискриминант ФИШЕРА
+# https://russianblogs.com/article/94131183549/
+
+# Тут тоже есть хорошие данные которые больше похожи на то что нахывается дискриминантом ФИШЕРА
+# https://question-it.com/questions/479815/linejnyj-diskriminant-fishera-v-python
+
+# Тут реализован код из 1 ссылки
+def LDA(x, y):      # x: all the input vector   y: labels
+   x_1 = np.array([x[i] for i in range(len(x)) if y[i] == 1])
+   x_2 = np.array([x[i] for i in range(len(x)) if y[i] == -1])
+
+   mju1 = np.mean(x_1, axis=0)     # mean vector
+   mju2 = np.mean(x_2, axis=0)
+
+   sw1 = np.dot((x_1 - mju1).T, (x_1 - mju1))    # Within-class scatter matrix
+   sw2 = np.dot((x_2 - mju2).T, (x_2 - mju2))
+   sw = sw1 + sw2
+
+   return np.dot(np.linalg.inv(sw), (mju1 - mju2))
+
+mean_1 = (-5, 0)   
+mean_2 = (5, 0)
+cov = [[1, 0],    
+       [0, 1]]
+size = 200          
+
+np.random.seed(1)
+x_1 = np.random.multivariate_normal(mean_1, cov, size)
+
+np.random.seed(2)
+x_2 = np.random.multivariate_normal(mean_2, cov, size)
+
+x = np.vstack((x_1, x_2))
+y = [-1] * 200 + [1] * 200      
+
+plt.scatter(x_1[:, 0], x_1[:, 1], color='blue', marker='o', label='Positive')
+plt.scatter(x_2[:, 0], x_2[:, 1], color='red', marker='x', label='Negative')
+plt.legend(loc='upper left')
+plt.title('Original Data')
+
+
+w = LDA(x, y)
+
+x1 = 1
+y1 = -1 / w[1] * (w[0] * x1)
+
+x2 = -1
+y2 = -1 / w[1] * (w[0] * x2)
+
+plt.plot([x1, x2], [y1, y2], 'r')
+plt.show()
+
+print(w)
+
+
+ """
